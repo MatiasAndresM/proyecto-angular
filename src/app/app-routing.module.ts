12a3fa568c79c8ec3973ffc84053ddce0496b4ff
MatiasAndresM <matiasa.m@hotmail.com>
2023-09-11 16:01:01 -0300

    TP-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ProductosComponent } from './productos/productos.component';
+import { RegistroComponent } from './registro/registro.component';
+import { AuricularesComponent } from './auriculares/auriculares.component';
+import { CelularesComponent } from './celulares/celulares.component';
+import { ComputadorasComponent } from './computadoras/computadoras.component';
+import { TabletsComponent } from './tablets/tablets.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'productos', component: ProductosComponent},
+  {path: 'registro', component: RegistroComponent},
+  {path: 'auriculares', component: AuricularesComponent},
+  {path: 'celulares', component: CelularesComponent},
+  {path: 'computadoras', component: ComputadorasComponent},
+  {path: 'tablets', component: TabletsComponent},
+  {path: 'carrito', component: CarritoComponent},
+  {path: "", redirectTo: "/inicio", pathMatch: "full"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ProductosComponent } from './productos/productos.component';
+import { RegistroComponent } from './registro/registro.component';
+import { AuricularesComponent } from './auriculares/auriculares.component';
+import { CelularesComponent } from './celulares/celulares.component';
+import { ComputadorasComponent } from './computadoras/computadoras.component';
+import { TabletsComponent } from './tablets/tablets.component';
+import { CarritoComponent } from './carrito/carrito.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent,
+    ProductosComponent,
+    RegistroComponent,
+    AuricularesComponent,
+    CelularesComponent,
+    ComputadorasComponent,
+    TabletsComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
